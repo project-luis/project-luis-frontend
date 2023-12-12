71446ddecb1820540b1a93fe47224e614724b9eb
Mert Toprak <mert.tprk@gmail.com>
2023-12-12 19:45:07 +0100

    edit bootcamp functionality works but doesn't visible until refresh

--- src/components/EditBootcampPopup.jsx
+++ src/components/EditBootcampPopup.jsx
@@ -1,254 +1,193 @@
 import axios from 'axios';
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { useNavigate, Link, useParams } from 'react-router-dom';
-
-const API_URL = 'http://localhost:5005';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function EditBootcampPopup(props) {
-	const [bootcamp, setBootcamp] = useState('');
-	const [name, setName] = useState('');
-	const [description, setDescription] = useState('');
-	const [teacher, setTeacher] = useState('');
-	const [avatarUrl, setAvatarUrl] = useState('');
-	const [languages, setLanguages] = useState(0);
-	const [bootcampCode, setBootcampCode] = useState('');
-	const [hoursPerWeek, setHoursPerWeek] = useState('');
-	const [courseLength, setCourseLength] = useState('');
-	const [startDate, setStartDate] = useState('dd - MM - yyyy');
-	const [endDate, setEndDate] = useState('');
-	const [daysOfWeek, setDaysOfWeek] = useState('');
-	const [modules, setModules] = useState('');
-	const [errorMessage, setErrorMessage] = useState('');
+	const [bootcamp, setBootcamp] = useState({
+		name: '',
+		description: '',
+		teacher: '',
+		avatarUrl: '',
+		languages: '',
+		bootcampCode: '',
+		hoursPerWeek: '',
+		courseLength: '',
+		startDate: 'dd - MM - yyyy',
+		endDate: '',
+		daysOfWeek: '',
+		modules: '',
+	});
 
+	const [errorMessage, setErrorMessage] = useState('');
 	const { bootcampId } = useParams();
 
-	const navigate = useNavigate();
-
-	const handleName = (e) => {
-		setName(e.target.value);
-	};
-
-	const handleDescription = (e) => {
-		setDescription(e.target.value);
-	};
-
-	const handleTeacher = (e) => {
-		setTeacher(e.target.value);
-	};
-
-	const handleAvatarUrl = (e) => {
-		setAvatarUrl(e.target.value);
-	};
-
-	const handleLanguages = (e) => {
-		setLanguages(e.target.value);
-	};
-
-	const handleBootcampCode = (e) => {
-		setBootcampCode(e.target.value);
-	};
-
-	const handleHoursPerWeek = (e) => {
-		setHoursPerWeek(e.target.value);
-	};
-
-	const handleCourseLength = (e) => {
-		setCourseLength(e.target.value);
-	};
-
-	const handleStartDate = (e) => {
-		setStartDate(e.target.value);
-	};
-
-	const handleEndDate = (e) => {
-		setEndDate(e.target.value);
-	};
-
-	const handleDaysOfWeek = (e) => {
-		setDaysOfWeek(e.target.value);
-	};
-
-	const handleModules = (e) => {
-		setModules(e.target.value);
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setBootcamp((prevBootcamp) => ({
+			...prevBootcamp,
+			[name]: value,
+		}));
 	};
 
 	const getBootcampFromApi = () => {
-		useEffect(() => {
-			const storedToken = localStorage.getItem('authToken');
-			axios
-				.get(`${import.meta.env.VITE_API_URL}/bootcamps/${bootcampId}`, {
-					headers: { Authorization: `Bearer ${storedToken}` },
-				})
-				.then((response) => {
-					setBootcamp(response.data);
-					console.log(`API: Connection success: ${response}`);
-				})
-				.catch((error) => {
-					console.log(`API: Connection Failed: ${error}`);
-					console.log(bootcampId);
-				});
-		}, []);
+		const storedToken = localStorage.getItem('authToken');
+		axios
+			.get(`${import.meta.env.VITE_API_URL}/bootcamps/${bootcampId}`, {
+				headers: { Authorization: `Bearer ${storedToken}` },
+			})
+			.then((response) => {
+				const bootcampData = Array.isArray(response.data)
+					? response.data[0]
+					: response.data;
+				setBootcamp(bootcampData);
+				console.log(`API: Connection success: ${response}`);
+				console.log('Updated bootcamp state:', bootcampData);
+			})
+			.catch((error) => {
+				console.log(`API: Connection Failed: ${error}`);
+				console.log(bootcampId);
+			});
 	};
 	const handleSubmit = (e) => {
 		e.preventDefault();
-
-		const requestBody = {
-			name,
-			description,
-			teacher,
-			avatarUrl,
-			languages,
-			bootcampCode,
-			hoursPerWeek,
-			courseLength,
-			startDate,
-			endDate,
-			daysOfWeek,
-			modules,
-		};
 
 		const storedToken = localStorage.getItem('authToken');
 
 		axios
 			.put(
 				`${import.meta.env.VITE_API_URL}/bootcamps/${bootcampId}`,
-				requestBody,
+				bootcamp,
 				{
 					headers: { Authorization: `Bearer ${storedToken}` },
 				}
 			)
 			.then((response) => {
-				console.log('Edit successful');
+				console.log('Edit successful', response.data);
+				setBootcamp(response.data);
 				props.setTrigger(false);
 			})
 			.catch((error) => {
 				console.log('Connection & Edit Failed?');
 				console.log(error);
-				const errorDescripton = error.response.data.message;
+				const errorDescripton = error.response?.data?.message || 'Edit failed';
 				setErrorMessage(errorDescripton);
 			});
 	};
 
-	getBootcampFromApi();
+	useEffect(() => {
+		getBootcampFromApi();
+	}, [bootcampId]);
+
+	const handleRefresh = () => {
+		getBootcampFromApi();
+	};
 
 	return props.trigger ? (
 		<div className="edit-bootcamp-popup">
 			<div className="bootcamp-popup-inner">
 				<h1>Edit This Bootcamp</h1>
-
 				<form className="edit-bootcamp-form" onSubmit={handleSubmit}>
 					<label>Name of The Bootcamp:</label>
 					<input
 						type="text"
-						placeholder={bootcamp.name}
 						name="name"
-						value={name}
-						onChange={handleName}
+						value={bootcamp.name || ''}
+						onChange={handleInputChange}
 					/>
 
 					<label>Description:</label>
-					<input
+					<textarea
 						type="textarea"
-						placeholder={bootcamp.description}
 						name="description"
-						value={description}
-						onChange={handleDescription}
+						value={bootcamp.description || ''}
+						onChange={handleInputChange}
+						rows={5}
 					/>
 
 					<label>Teacher:</label>
 					<input
 						type="text"
-						placeholder={bootcamp.teacher}
 						name="teacher"
-						value={teacher}
-						onChange={handleTeacher}
+						value={bootcamp.teacher || ''}
+						onChange={handleInputChange}
 					/>
 
 					<label>Logo of the Bootcamp:</label>
 					<input
 						type="text"
-						placeholder={bootcamp.avatarUrl}
 						name="avatarUrl"
-						value={avatarUrl}
-						onChange={handleAvatarUrl}
+						value={bootcamp.avatarUrl || ''}
+						onChange={handleInputChange}
 					/>
 
 					<label>Languages: </label>
 					<input
 						type="text"
-						placeholder={bootcamp.languages}
 						name="languages"
-						value={languages}
-						onChange={handleLanguages}
+						value={bootcamp.languages || ''}
+						onChange={handleInputChange}
 					/>
 
 					<label>Bootcamp Code</label>
 					<input
 						type="text"
-						placeholder={bootcamp.bootcampCode}
 						name="bootcampCode"
-						value={bootcampCode}
-						onChange={handleBootcampCode}
+						value={bootcamp.bootcampCode || ''}
+						onChange={handleInputChange}
 					/>
 
 					<label>Hours Per Week?</label>
 					<input
 						type="number"
-						placeholder={bootcamp.hoursPerWeek}
 						name="hoursPerWeek"
-						value={hoursPerWeek}
-						onChange={handleHoursPerWeek}
+						value={bootcamp.hoursPerWeek || ''}
+						onChange={handleInputChange}
 					/>
 
 					<label>Length of Course: </label>
 					<input
 						type="number"
-						placeholder={bootcamp.courseLength}
 						name="courseLength"
-						value={courseLength}
-						onChange={handleCourseLength}
+						value={bootcamp.courseLength || ''}
+						onChange={handleInputChange}
 					/>
 
 					<label>Start Date: </label>
 					<input
 						type="date"
-						placeholder={bootcamp.startDate}
 						name="startDate"
-						value={startDate}
-						onChange={handleStartDate}
+						value={bootcamp.startDate || ''}
+						onChange={handleInputChange}
 					/>
 
 					<label>End Date: </label>
 					<input
 						type="date"
-						placeholder={bootcamp.endDate}
 						name="endDate"
-						value={endDate}
-						onChange={handleEndDate}
+						value={bootcamp.endDate || ''}
+						onChange={handleInputChange}
 					/>
 
 					<label>How many days per week?: </label>
 					<input
 						type="number"
-						placeholder={bootcamp.daysOfWeek}
 						name="daysofWeek"
-						value={daysOfWeek}
-						onChange={handleDaysOfWeek}
+						value={bootcamp.daysOfWeek || ''}
+						onChange={handleInputChange}
 					/>
 
-					<label>Which Modules are included to this Bootcamp? </label>
+					<label>Which Modules are included in this Bootcamp? </label>
 					<input
-						type="string"
-						placeholder={[...bootcamp.modules].map((module) => {
-							return ' ' + module.name;
-						})}
-						name="modules"
-						value={modules}
-						onChange={handleModules}
+						type="text"
+						value={
+							bootcamp.modules.map((module) => module.name).join(', ') || ''
+						}
+						onChange={handleInputChange}
 					/>
 
-					<button type="submit">Submit</button>
+					<button type="submit" onClick={handleRefresh}>
+						Submit
+					</button>
 				</form>
 				{errorMessage && <p className="error-message">{errorMessage}</p>}
 			</div>
