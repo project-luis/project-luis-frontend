9579a4054b740836cbede865e724592cc9f51951
Mert Toprak <mert.tprk@gmail.com>
2023-12-15 13:09:06 +0100

    fixed few css

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -30,7 +30,7 @@ function ProfilePage(props) {
 	};
 
 	const getBootcampData = () => {
-		const storedToken = localStorage.getItem("authToken");
+		const storedToken = localStorage.getItem('authToken');
 
 		axios
 			.get(`${import.meta.env.VITE_API_URL}/bootcamps`, {
@@ -59,8 +59,9 @@ function ProfilePage(props) {
 
 			<div className="profile-page">
 				<button
-				className="function-btn"
-				onClick={() => setEditProfileButton(true)}>
+					className="function-btn"
+					onClick={() => setEditProfileButton(true)}
+				>
 					Edit Profile
 				</button>
 
@@ -68,33 +69,45 @@ function ProfilePage(props) {
 					{[userProfile].map((userItem, i) => {
 						return (
 							<div key={i}>
-								<div className="profile-detail-card-container">
-									<div className="profile-detail-card-content">
-										<img
-											className="profile-avatar"
-											src={userItem.avatarUrl || "https://static.thenounproject.com/png/1876981-200.png"}
-										/>
-										<p>{userItem.fullName}</p>
-										<p>I speak: {userItem.languages}</p>
+								<div className="profile-and-contact">
+									<div className="profile-detail-card-container">
+										<div className="profile-detail-card-content">
+											<img
+												className="profile-avatar"
+												src={
+													userItem.avatarUrl ||
+													'https://static.thenounproject.com/png/1876981-200.png'
+												}
+											/>
+											<p>{userItem.fullName}</p>
+											<p>I speak: {userItem.languages}</p>
+										</div>
+									</div>
+
+									<div className="profile-contact-card-container">
+										<div className="profile-contact-card-content">
+											<a href={`mailto:${userItem.email}`}>Email</a>
+
+											<a
+												href={
+													userItem.linkedInUrl || 'https://www.linkedin.com/'
+												}
+												target="_blank"
+												rel="noreferrer"
+											>
+												LinkedIn
+											</a>
+
+											<a
+												href={userItem.githubUrl || 'https://github.com/'}
+												target="_blank"
+												rel="noreferrer"
+											>
+												GitHub
+											</a>
+										</div>
 									</div>
 								</div>
-
-								<div className="profile-contact-card-container">
-									<div className="profile-contact-card-content">
-										<a href={`mailto:${userItem.email}`}>Email</a>
-
-										<a href={userItem.linkedInUrl || "https://www.linkedin.com/"}
-											target="_blank"
-											rel="noreferrer">LinkedIn
-										</a>
-
-										<a href={userItem.githubUrl || "https://github.com/"}
-											target="_blank"
-											rel="noreferrer">GitHub
-										</a>
-									</div>
-								</div>
-
 								<div className="profile-about-card-container">
 									<div className="profile-about-card-content">
 										<p>My bootcamps:</p>
@@ -111,8 +124,16 @@ function ProfilePage(props) {
 												</div>
 											);
 										})}
-										<p>My experience and expertise:<br />{userItem.field}</p>
-										<p>About me:<br />{userItem.aboutUser}</p>
+										<p>
+											My experience and expertise:
+											<br />
+											{userItem.field}
+										</p>
+										<p>
+											About me:
+											<br />
+											{userItem.aboutUser}
+										</p>
 									</div>
 								</div>
 							</div>
@@ -123,9 +144,8 @@ function ProfilePage(props) {
 				<EditProfilePopup
 					trigger={editProfileButton}
 					setTrigger={setEditProfileButton}
-					updateUserData={updateUserData}>
-				</EditProfilePopup>
-
+					updateUserData={updateUserData}
+				></EditProfilePopup>
 			</div>
 		</>
 	);
